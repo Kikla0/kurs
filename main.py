a2bc36fd9bd3a0f823db05bf09a81c12f6be3b09
Kikla0 <Kikla2662003@gmail.com>
2021-09-21 19:31:52 +0200

    poprawka

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-print("Rozwiązywanie równania kwadratowego y=ax^2+bx+c")
+print("Rozwiązywanie równania kwadratowego y = ax^2 + bx + c")
 
 a = float(input("Podaj a: "))
 b = float(input("Podaj b: "))
 c = float(input("Podaj c: "))
 
-delta = (b**2)-(4*a*c)
+delta = (b*b)-(4*a*c)
 
 if delta < 0:
     print("Nie ma pierwiastków")
@@ -13,8 +13,8 @@ if delta == 0:
     x = (b*-1)/(2*a)
     print(f"Równanie ma jeden pierwiastek: {x}")
 
-if delta > 10:
+if delta > 0:
     delta = delta**(1/2)
-    x1 = ((b*-1) - delta)/2*a
-    x2 = ((b*-1) + delta)/2*a
+    x1 = ((b*-1) - delta)/(2*a)
+    x2 = ((b*-1) + delta)/(2*a)
     print(f"Równanie ma dwa pierwiastki: {x1} i {x2}")
